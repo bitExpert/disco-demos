fd0e6f94d0322077639b13b3f076288faca3b3c0
Stephan Hochdörfer <S.Hochdoerfer@bitExpert.de>
2016-09-02 19:06:00 +0200

    Ported Slim's DefaultServicesProvider config to Disco format.

--- src/SlimDemo/Config.php
+++ src/SlimDemo/Config.php
@@ -1,12 +1,136 @@
 <?php
+declare(strict_types = 1);
 
 namespace SlimDemo;
 
+use bitExpert\Disco\Annotations\Bean;
 use bitExpert\Disco\Annotations\Configuration;
+use bitExpert\Disco\BeanFactoryRegistry;
+use Slim\CallableResolver;
+use Slim\Handlers\Error;
+use Slim\Handlers\NotAllowed;
+use Slim\Handlers\NotFound;
+use Slim\Handlers\PhpError;
+use Slim\Handlers\Strategies\RequestResponse;
+use Slim\Http\Environment;
+use Slim\Http\Headers;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Router;
 
 /**
  * @Configuration
  */
 class Config
 {
+    /**
+     * @Bean
+     */
+    public function settings() : array
+    {
+        return [
+            'httpVersion' => '1.1',
+            'responseChunkSize' => 4096,
+            'outputBuffering' => 'append',
+            'determineRouteBeforeAppMiddleware' => false,
+            'displayErrorDetails' => false,
+            'addContentLengthHeader' => true,
+            'routerCacheFile' => false,
+        ];
+    }
+
+    /**
+     * @Bean
+     */
+    public function environment() : Environment
+    {
+        return new Environment($_SERVER);
+    }
+
+    /**
+     * @Bean
+     */
+    public function request() : Request
+    {
+        $beanFactory = BeanFactoryRegistry::getInstance();
+
+        return Request::createFromEnvironment($beanFactory->get('environment'));
+    }
+
+    /**
+     * @Bean
+     */
+    public function response() : Response
+    {
+        $beanFactory = BeanFactoryRegistry::getInstance();
+        $headers = new Headers(['Content-Type' => 'text/html; charset=UTF-8']);
+        $response = new Response(200, $headers);
+
+        return $response->withProtocolVersion($beanFactory->get('settings')['httpVersion']);
+    }
+
+    /**
+     * @Bean
+     */
+    public function router() : Router
+    {
+        $beanFactory = BeanFactoryRegistry::getInstance();
+        $routerCacheFile = false;
+        if (isset($beanFactory->get('settings')['routerCacheFile'])) {
+            $routerCacheFile = $beanFactory->get('settings')['routerCacheFile'];
+        }
+
+        return (new Router)->setCacheFile($routerCacheFile);
+    }
+
+    /**
+     * @Bean
+     */
+    public function foundHandler() :RequestResponse
+    {
+        return new RequestResponse;
+    }
+
+    /**
+     * @Bean
+     */
+    public function phpErrorHandler() : PhpError
+    {
+        $beanFactory = BeanFactoryRegistry::getInstance();
+        return new PhpError($beanFactory->get('settings')['displayErrorDetails']);
+    }
+
+    /**
+     * @Bean
+     */
+    public function errorHandler() : Error
+    {
+        $beanFactory = BeanFactoryRegistry::getInstance();
+
+        return new Error($beanFactory->get('settings')['displayErrorDetails']);
+    }
+
+    /**
+     * @Bean
+     */
+    public function notFoundHandler() : NotFound
+    {
+        return new NotFound();
+    }
+
+    /**
+     * @Bean
+     */
+    public function notAllowedHandler() : NotAllowed
+    {
+        return new NotAllowed();
+    }
+
+    /**
+     * @Bean
+     */
+    public function callableResolver() : CallableResolver
+    {
+        return new CallableResolver(BeanFactoryRegistry::getInstance());
+    }
 }
